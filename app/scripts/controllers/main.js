57e164adbea8504bfd005666e2948572ab17ff21
shyndman <scotty.hyndman@gmail.com>
2013-09-08 10:20:52 -0400

    Scaffold with updated Angular and Bootstrap dependencies

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('deckBuilderApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
