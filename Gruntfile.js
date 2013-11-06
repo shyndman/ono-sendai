5a25af7852e1d90730eb169608ee17f50944cbb8
shyndman <scotty.hyndman@gmail.com>
2013-11-06 12:31:02 -0500

    We now bump a data/version.json file, so we can read the value from the app

--- Gruntfile.js
+++ Gruntfile.js
@@ -365,7 +365,7 @@ module.exports = function (grunt) {
     },
     bump: {
       options: {
-        files: ['package.json', 'bower.json']
+        files: ['package.json', 'bower.json', 'app/data/version.json']
       }
     }
   });
