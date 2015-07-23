6edb450f5eb3a4b753a725ba9774ec70a7aba95d
Ryan Myers <ryan.p.myers@gmail.com>
2015-07-23 16:15:00 -0400

    Remove unused attributes, and clean up the code for the python importer

--- script/card_update.py
+++ script/card_update.py
@@ -14,24 +14,33 @@ def main():
   nrdb_json = urllib.request.urlopen(NRDB_URL).readall().decode('utf-8') #unicode_escape'?
   nrdb_cards = json.loads(nrdb_json)
   
-  #Remove Chronos Protocol cards
-  cards_to_remove = []
-  
-  for i in range(len(existing_cards['cards'])):
-    card = existing_cards['cards'][i]
-    if 'Chronos Protocol' in card['title']:
-      cards_to_remove.append(i)
-      
-  for i in cards_to_remove:
-    del existing_cards['cards'][i]
+  existing_cards['cards'] = RemoveChronosProtocol(existing_cards['cards'])
+  nrdb_cards = RemoveChronosProtocol(nrdb_cards)
   
   #Remove unused attributes from nrdb cards
-  attr_to_remove = ['code','set_code','side_code','faction_code','cyclenumber','limited','faction_letter','type_code','subtype_code','last-modified']
-  for card in nrdb_cards:
+  attr_to_remove = ['code','set_code','side_code','faction_code','cyclenumber','limited','faction_letter','type_code','subtype_code','last-modified','url']
+  for card in nrdb_cards:      
+    card['nrdb_url'] = card['url']
+    card['nrdb_art'] = card['imagesrc']
+    card['imagesrc'] = "/images/cards/" + card['title'] + ".png"
     for attr in attr_to_remove:
       del card[attr]
     
   print(nrdb_cards[0])
+  
+def RemoveChronosProtocol(cards):
+  cards_to_remove = []
+  
+  for i in range(len(cards)):
+    card = cards[i]
+    if 'Chronos Protocol' in card['title']:
+      cards_to_remove.append(i)
+      
+  for i in cards_to_remove:
+    del cards[i]
+    
+  return cards
+  
  
 if __name__ == "__main__":
   main()
