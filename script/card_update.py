a7e4bde838a65f126bdfe30be35ac37eed867d7b
Ryan Myers <ryan.p.myers@gmail.com>
2015-07-24 11:49:51 -0400

    Save the card images form NRDB

--- script/card_update.py
+++ script/card_update.py
@@ -4,10 +4,21 @@ import urllib.request
 import json
 import re
 from wand.image import Image
+#from wand.api import library
+import ctypes
 
 NRDB_URL = "http://netrunnerdb.com/api/cards/"
 EXISTING_CARDS_PATH = 'app/data/cards.json'
 
+# Register C-type arguments
+#library.MagickQuantizeImage.argtypes = [ctypes.c_void_p,
+#                                        ctypes.c_size_t,
+#                                        ctypes.c_int,
+#                                        ctypes.c_size_t,
+#                                        ctypes.c_int,
+#                                        ctypes.c_int
+#                                       ]   
+#library.MagickQuantizeImage.restype = None
 
 def main():
     existing_cards_file = open(EXISTING_CARDS_PATH, encoding='utf-8')
@@ -32,18 +43,29 @@ def main():
         for attr in attr_to_remove:
             del card[attr]
 
-        image_response = urllib.request.urlopen('http://netrunnerdb.com/' + card['nrdb_art'])
-
-        try:
-            with Image(file=image_response) as img:
-                print('format =', img.format)
-                print('size =', img.size)
-        finally:
-            image_response.close()
+        save_card_image(card['nrdb_art'], card['imagesrc'])
 
         del card['nrdb_art']
 
     print(nrdb_cards[0])
+    
+def save_card_image(url_to_card, path_to_save):
+    image_response = urllib.request.urlopen('http://netrunnerdb.com/' + url_to_card)
+    try:
+        with Image(file=image_response, format='png') as img:
+            # Update the image settings
+            #color_count = 256
+            #colorspace = 1 # RGB
+            #treedepth = 8                
+            #dither = 1 # True
+            #merror = 0 # False
+            #library.MagickQuantizeImage(img.wand,color_count,colorspace,treedepth,dither,merror)
+            
+            # Save the new image
+            img.save(filename='app' + path_to_save)
+            print("Saving - app" + path_to_save)
+    finally:
+        image_response.close()
 
 
 def remove_chronos_protocol(cards):
